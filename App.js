481a3da5a24380809d1403dcf6aa8db294b3f4cb
Jayanti Jagannath <jagannath.jyanti5@gmail.com>
2020-11-01 23:55:21 +0530

    basic layout

--- App.js
+++ App.js
@@ -1,15 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Button } from 'react-native';
 
 export default function App() {
+  
+  const [description, setDescription] = useState('')
+  const [amount, setAmount] = useState(0)
+  const [details, setDetails] = useState([{
+    description: "Welcome, hope your earnings increase every day.",
+    amount: 0
+  }])
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      
+      <Text>What You Have</Text>
+    
+      <TextInput
+        placeholder = 'What did you do today?'
+        stlye={styles.input} />
+      
+      <TextInput
+        placeholder = 'How much did you earn?'
+        stlye={styles.input} />
+      
+      <Button title = "Add event"/>
+     
+      
+    
     </SafeAreaView>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +43,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  input: {
+    height: 40,
+    borderColor: 'skyblue',
+    borderWidth: 1,
+    margin:20
+  }
+})
